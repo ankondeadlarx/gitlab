39f07085eaa40ad8692078f06062090c924e3646
Scott Brown <scott.brown@ucalgary.ca>
2025-03-03 09:06:41 -0700

    Updated TicTacToe.java 'magic' characters and numbers replaced them with static variables universal methods and variables moved to AbstractGame

--- src/core/gamelogic/tictactoe/TicTacToe.java
+++ src/core/gamelogic/tictactoe/TicTacToe.java
@@ -1,8 +1,8 @@
 public class TicTacToe extends AbstractGame {
 
-    private String board;
-    private Player player1;
-    private Player player2;
+    //private String board;
+    private static final char PLAYER_ONE_CHAR = 'X';
+    private static final char PLAYER_TWO_CHAR = 'O';
 
     // the board will be stored in a string in the following format:
     // "123456789" where 1-9 will be X, for player 1, O for player 2 or
@@ -13,7 +13,7 @@ public class TicTacToe extends AbstractGame {
     // | 7 | 8 | 9 |
     // '---'---'---'
 
-    // default constructor
+    // default constructor for a TicTacToe game
     public TicTacToe (Player player1, Player player2) {
         this.board = "123456789";
         this.player1 = player1;
@@ -22,38 +22,40 @@ public class TicTacToe extends AbstractGame {
 
 
     // for placing an X or O on the board
-    public boolean place(char placement, int position) {
-        if(this.board.charAt(position)=='X' ||
-           this.board.charAt(position)=='O') {
-            return false;
+    public void place(Player turn, int position) {
+        if(this.board.charAt(position)==PLAYER_ONE_CHAR ||
+           this.board.charAt(position)==PLAYER_TWO_CHAR) {
+            throw new IllegalMoveExeption();
         }
-        this.board = this.board.substring(0,position)+placement+this.board.substring(position+1);
-        return true;
+        if(turn==player1) {
+            this.board = this.board.substring(0, position) + PLAYER_ONE_CHAR + this.board.substring(position + 1);
+        } else if(turn==player2) {
+            this.board = this.board.substring(0, position) + PLAYER_TWO_CHAR + this.board.substring(position + 1);
+        } else {
+            throw new PlayerException();
+        }
     }
 
-    public String getBoard(){
-        return board;
-    }
-
+    // returns the winning player, null if the game does not yet have a winner
     public Player getWinner(){
-        if(this.board.charAt(0)=='X' && this.board.charAt(0)==this.board.charAt(1) && this.board.charAt(1)==this.board.charAt(2) ||
-                this.board.charAt(3)=='X' && this.board.charAt(3)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(5) ||
-                this.board.charAt(6)=='X' && this.board.charAt(6)==this.board.charAt(7) && this.board.charAt(7)==this.board.charAt(8) ||
-                this.board.charAt(0)=='X' && this.board.charAt(0)==this.board.charAt(3) && this.board.charAt(3)==this.board.charAt(6) ||
-                this.board.charAt(1)=='X' && this.board.charAt(1)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(7) ||
-                this.board.charAt(2)=='X' && this.board.charAt(2)==this.board.charAt(5) && this.board.charAt(5)==this.board.charAt(8) ||
-                this.board.charAt(0)=='X' && this.board.charAt(0)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(8) ||
-                this.board.charAt(2)=='X' && this.board.charAt(2)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(6) ||
+        if(this.board.charAt(0)==PLAYER_ONE_CHAR && this.board.charAt(0)==this.board.charAt(1) && this.board.charAt(1)==this.board.charAt(2) ||
+                this.board.charAt(3)==PLAYER_ONE_CHAR && this.board.charAt(3)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(5) ||
+                this.board.charAt(6)==PLAYER_ONE_CHAR && this.board.charAt(6)==this.board.charAt(7) && this.board.charAt(7)==this.board.charAt(8) ||
+                this.board.charAt(0)==PLAYER_ONE_CHAR && this.board.charAt(0)==this.board.charAt(3) && this.board.charAt(3)==this.board.charAt(6) ||
+                this.board.charAt(1)==PLAYER_ONE_CHAR && this.board.charAt(1)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(7) ||
+                this.board.charAt(2)==PLAYER_ONE_CHAR && this.board.charAt(2)==this.board.charAt(5) && this.board.charAt(5)==this.board.charAt(8) ||
+                this.board.charAt(0)==PLAYER_ONE_CHAR && this.board.charAt(0)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(8) ||
+                this.board.charAt(2)==PLAYER_ONE_CHAR && this.board.charAt(2)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(6) ||
                 !player2.isOnline()){
             return player1;
-        } else if(this.board.charAt(0)=='O' && this.board.charAt(0)==this.board.charAt(1) && this.board.charAt(1)==this.board.charAt(2) ||
-                this.board.charAt(3)=='O' && this.board.charAt(3)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(5) ||
-                this.board.charAt(6)=='O' && this.board.charAt(6)==this.board.charAt(7) && this.board.charAt(7)==this.board.charAt(8) ||
-                this.board.charAt(0)=='O' && this.board.charAt(0)==this.board.charAt(3) && this.board.charAt(3)==this.board.charAt(6) ||
-                this.board.charAt(1)=='O' && this.board.charAt(1)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(7) ||
-                this.board.charAt(2)=='O' && this.board.charAt(2)==this.board.charAt(5) && this.board.charAt(5)==this.board.charAt(8) ||
-                this.board.charAt(0)=='O' && this.board.charAt(0)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(8) ||
-                this.board.charAt(2)=='O' && this.board.charAt(2)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(6) ||
+        } else if(this.board.charAt(0)==PLAYER_TWO_CHAR && this.board.charAt(0)==this.board.charAt(1) && this.board.charAt(1)==this.board.charAt(2) ||
+                this.board.charAt(3)==PLAYER_TWO_CHAR && this.board.charAt(3)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(5) ||
+                this.board.charAt(6)==PLAYER_TWO_CHAR && this.board.charAt(6)==this.board.charAt(7) && this.board.charAt(7)==this.board.charAt(8) ||
+                this.board.charAt(0)==PLAYER_TWO_CHAR && this.board.charAt(0)==this.board.charAt(3) && this.board.charAt(3)==this.board.charAt(6) ||
+                this.board.charAt(1)==PLAYER_TWO_CHAR && this.board.charAt(1)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(7) ||
+                this.board.charAt(2)==PLAYER_TWO_CHAR && this.board.charAt(2)==this.board.charAt(5) && this.board.charAt(5)==this.board.charAt(8) ||
+                this.board.charAt(0)==PLAYER_TWO_CHAR && this.board.charAt(0)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(8) ||
+                this.board.charAt(2)==PLAYER_TWO_CHAR && this.board.charAt(2)==this.board.charAt(4) && this.board.charAt(4)==this.board.charAt(6) ||
                 !player1.isOnline()){
             return player2;
         } else {
@@ -61,20 +63,8 @@ public class TicTacToe extends AbstractGame {
         }
     }
 
-    public Player getLoser(){
-        Player winner = getWinner();
-        if(player1==winner){
-            return player2;
-        } else if(player2==winner){
-            return player1;
-        }
-        return null;
-    }
 
-    public boolean gameIsOver() {
-        return drew() || getWinner() != null || playerQuit();
-    }
-
+    // Whether or not the game is tied
     public boolean drew() {
         if(getWinner()==null){
             return false;
@@ -90,8 +80,6 @@ public class TicTacToe extends AbstractGame {
                 this.board.charAt(8) != '9';
     }
 
-    public boolean playerQuit(){
-        return !player1.isOnline() || !player2.isOnline();
-    }
+
 
 }
